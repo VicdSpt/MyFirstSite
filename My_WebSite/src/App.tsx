ec4c2f2b4625f9a1fe151c2ca9fab1e7f9bd5586
Victor dS <victordespirlet@gmail.com>
2026-01-29 00:35:33 +0100

    updates

--- My_WebSite/src/App.tsx
+++ My_WebSite/src/App.tsx
@@ -1,12 +1,24 @@
+import { useEffect } from "react";
 import "./App.css";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 
 import SectionHeader from "./components/SectionHeader.tsx";
 import SectionFooter from "./components/SectionFooter.tsx";
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <SectionHeader />
       <main className="grow">
         <Outlet />
